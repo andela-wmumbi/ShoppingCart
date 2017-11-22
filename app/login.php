03dd2d0546e4a70da74fdea20c64d640ff61b315
andela-wmumbi <winnie.mumbi@andela.com>
2017-11-22 16:41:06 +0300

    ch(login): fixed login page   - fixed login page design   - added password verification
    [Delivers #152963500]

--- app/login.php
+++ app/login.php
@@ -1,57 +1,73 @@
 <?php
-
 require "../includes/session.php";
 require "../includes/db.php";
 require "../includes/utils.php";
 
+$emailErr = $passwordErr = $valErr = $error = "";
 if (loggedIn()) {
     redirect('index.php');
 }
 if (!empty($_POST)) {
+    empty(trim($_POST['email'])) ? $emailErr = "Email required" : "";
+    empty(trim($_POST['password'])) ? $passwordErr = "Password required" : "";
+    (!empty(trim($_POST['email'])) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) ? $valErr = "Enter a valid email" : "";
     $email = $_POST['email'];
     $password = $_POST['password'];
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        if (!empty($password)) {
+
+    $sql = "SELECT password FROM users WHERE email=:email";
+    $stmt = $connect->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $result = $stmt->fetchColumn();
+    if ($result) {
+        if (password_verify($password, $result)) {
             $sql = "SELECT id FROM users WHERE email=:email AND password=:password";
             $auth = $connect->prepare($sql);
             $auth->bindValue(':email', $email);
             $auth->bindValue(':password', $password);
             $auth->execute();
 
-            $result = $auth->fetchColumn();
+            $id = $auth->fetchColumn();
+            logIn($id);
+            redirect('index.php');
 
-            if ($result) {
-                logIn($result);
-                redirect('index.php');
-            }
-            echo "User not found";
+        } else {
+            $error = "Wrong Password";
         }
+    } else {
+          $error = "User not found";
     }
 }
 ?>
 <html>
-    <head>
-        <link rel="stylesheet" href="main.css" />
-        <link rel="stylesheet" href="css/bootstrap.min.css" />
-    </head>
-    <body>
-    <div class="loginform">
-        <form method="post" class="form">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div>
-            <button type="submit" name="formSubmit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-        <p class="link" style="margin-top: 10px; line-height: 2.0;">
-        <a href="./register.php">Create Account</a>
-        </p>
+<head>
+    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body>
+<div class="create">Login into your account</div>
+<div class="loginform">
+  <div class="err"><span class="error"><?php echo $error;?></span></div>
+  <form method="post" class="form">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" name="email" class="form-control"
+        id="exampleInputEmail1" aria-describedby="emailHelp"
+        placeholder="Enter email">
+      <span class="error">* <?php echo $emailErr;?><?php echo $valErr;?></span>
     </div>
-    </body>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" name="password" class="form-control"
+        id="exampleInputPassword1" placeholder="Password">
+      <span class="error">* <?php echo $passwordErr;?></span>
+    </div>
+    <div>
+    <button type="submit" name="formSubmit" class="btn btn-primary">Submit</button>
+    <a href="./register.php">Create Account</a>
+    </div>
+  </form>
+  </p>
+</div>
+</body>
 </html>
